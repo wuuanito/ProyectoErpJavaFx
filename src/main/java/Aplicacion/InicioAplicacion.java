e32813cb1ca7a887465bfe4b228f06d7610bf7d9
Juan <juanfrauca@outlook.com>
2024-01-18 20:40:37 +0100

    Initial commit

---
+++ src/main/java/Aplicacion/InicioAplicacion.java
@@ -0,0 +1,15 @@
+package Aplicacion;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InicioAplicacion extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
---
+++ src/main/java/Controlador/EmpleadoCliente.java
@@ -0,0 +1,2 @@
+package Aplicacion;public class EmpleadoCliente {
+}
